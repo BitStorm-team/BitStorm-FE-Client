0a1f5b8a2cb85211ab9fa723fb9024b0cc4f471a
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-25 10:31:44 +0700

    change header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,11 +2,11 @@ import { Avatar, Button } from "antd";
 import Logo from "../components/Logo";
 import NavBar from "./NavBar";
 import "../assets/css/header.css";
-import { useState, useEffect } from "react"; // Import useEffect
+import { useState, useEffect } from "react";
 import { getStorage } from "../utils/helpers";
 import { UserOutlined } from "@ant-design/icons";
 import axios from "axios";
-import { jwtDecode } from "jwt-decode"; // Change import to import from 'jwt-decode'
+import {jwtDecode} from "jwt-decode"; // Corrected import
 
 const MainHeader = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -19,34 +19,34 @@ const MainHeader = () => {
     if (storedToken) {
       setToken(storedToken);
       setIsLogin(true); // Set isLogin to true if token exists
-      const decodedToken = jwtDecode(storedToken); // Use jwtDecode
+      const decodedToken = jwtDecode(storedToken); // Use jwtDecode correctly
       setUserInfo(decodedToken);
     }
   }, []);
 
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:8000/api/experts/profile/${userInfo.sub}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`, // Include the token in the request headers
-            },
-          }
-        );
-        setUserProfile(response.data.data); // Assuming response.data contains the user profile
-      } catch (error) {
-        localStorage.setItem("permission", true);
-        // handleLogout();
-      }
-    };
-
     if (userInfo && userInfo.sub) {
       getUser();
     }
   }, [userInfo, token]); // Add userInfo and token as dependencies
-  console.log(userProfile);
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/api/experts/profile/${userInfo.sub}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Include the token in the request headers
+          },
+        }
+      );
+      setUserProfile(response.data.data); // Assuming response.data.data contains the user profile
+    } catch (error) {
+      localStorage.setItem("permission", true);
+      // handleLogout();
+    }
+  };
+
   return (
     <header className="main-header">
       <div className="header-section logo">
@@ -64,7 +64,8 @@ const MainHeader = () => {
         ) : (
           <Avatar
             size={60}
-            src={userProfile.profile_picture}
+            src={userProfile?.profile_picture} // Use optional chaining
+            icon={<UserOutlined />}
           />
         )}
       </div>
