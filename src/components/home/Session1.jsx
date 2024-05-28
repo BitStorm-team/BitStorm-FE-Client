9c32639cb53c610beaabef730736b825418620ec
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-28 17:46:20 +0700

    update homepage

--- src/components/home/Session1.jsx
+++ src/components/home/Session1.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../../assets/css/home/session1.css";
 import doctorImage from "../../assets/images/doctor3D.png"; // Ensure you have the image in the src folder or adjust the path accordingly
+import 'aos/dist/aos.css';  // Import AOS styles
+import AOS from 'aos';
+import scrollDownImg from '../../assets/images/scrolldown-img.png';
 
 const Session1 = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS
+  }, []);
+
   return (
     <div className="home-container">
       <video className="video-background" autoPlay muted loop>
@@ -13,15 +20,15 @@ const Session1 = () => {
         Your browser does not support the video tag.
       </video>
 
-      <div className="text-content">
+      <div className="text-content" data-aos="fade-down">
         <h1>
           Chào Mừng Bạn Đến <span className="highlight">BitStorm</span>
         </h1>
         <p>Nơi lắng nghe những tâm sự của bạn</p>
         <button className="start-button">Bắt Đầu Ngay</button>
-      </div>
-      <div className="image-content">
-        <img data-aos="fade-up" src={doctorImage} alt="Doctor" />
+        <div className="scroll-down-icon">
+          <img style={{ width: '30%' }} src={scrollDownImg} alt="" />
+        </div>
       </div>
     </div>
   );
