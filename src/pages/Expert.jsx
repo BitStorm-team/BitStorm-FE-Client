f3f61802306072473a6f72d82ab56cf24db0fd46
thu <thu.vo25@student.passerellesnumeriques.org>
2024-06-02 11:54:56 +0700

    search

--- src/pages/Expert.jsx
+++ src/pages/Expert.jsx
@@ -1,20 +1,46 @@
-import {Card,Pagination } from "antd";
+import {Card,Pagination, message } from "antd";
 import '../assets/css/expert.css';
 import { StarTwoTone } from "@ant-design/icons";
-import { useState,useEffect } from "react";
+import { useState,useEffect,useParams } from "react";
 import axios from "axios";
+import { useInView } from 'react-intersection-observer';
 const Expert = () =>{
+    const [ref1, inView1] = useInView({ threshold: 0.5 });
     const [experts, setExperts] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 4;
+    const [expertName, setExpertName] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
+    const searchExpert = async (term) => {
+        try {
+            const res = await axios.post("http://127.0.0.1:8000/api/experts/search", { searchTerm: term });
+            setExpertName(res.data.data);
+            console.log(res.data.data);
+        } catch (error) {
+            console.error("Error fetching data", error);
+        }
+    }
+    useEffect(() => {
+        searchExpert()
+    },[])
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (searchTerm === "") {
+            message.error("Please enter something");
+        } else {
+            searchExpert(searchTerm);
+            setSearchTerm("");
+        }
+    }
+
+    const onChange = (e) => setSearchTerm(e.target.value);
     useEffect(() => {
         const fetchExperts = async () => {
             try {
                 const res = await axios.get("http://127.0.0.1:8000/api/experts");
                 console.log(res.data); // Log the entire response
-                setExperts(res.data.data[0]); 
-                console.log("agrg",experts)// Adjust this based on actual API response structure
+                setExperts(res.data.data[0]); // Adjust this based on actual API response structure
             } catch (error) {
                 console.error("Error", error);
             }
@@ -26,27 +52,27 @@ const Expert = () =>{
     const handleChange = (page) => {
         setCurrentPage(page);
     };
-
     // Calculate the items to display on the current page
     const indexOfLastItem = currentPage * itemsPerPage;
     const indexOfFirstItem = indexOfLastItem - itemsPerPage;
     const currentItem = experts.slice(indexOfFirstItem,indexOfLastItem);
-    console.log("ewfwrgdfg",currentItem);
-
-    console.log("Current experts:", experts);
     return (
         <div className="Expert">
             <div className="search">
                 <div className="wrapper1">
                     <div className="search-wrapper">
                         <label htmlFor="search-form">
+                        <form onSubmit={onSubmit} className="form">
                             <input
-                                type="search"
-                                name="search-form"
+                                type="text"
+                                name="searchTerm"
                                 id="search-form"
                                 className="search-input"
+                                value={searchTerm}
+                                onChange={onChange}
                                 placeholder="Search for..."
                             />
+                        </form>
                             {/* <span className="sr-only">Search countries here</span> */}
                         </label>
                         <div className="select">
@@ -61,13 +87,23 @@ const Expert = () =>{
                     </div>
                 </div>
             </div>
-        <div className="wrapper">
+            <div className="wrapper">
+                {expertName.map(item => (
+                        <div className="card" key={item.id}  animate={inView1}>
+                            <img className="card-image" src={item.profile_picture} alt={item.name} />
+                            <h2>{item.name}</h2>
+                            <p>{item.email}</p>
+                            <button className="custom-btn btn-16" >Read More</button>
+                        </div>
+                    ))}
+            </div>
+        <div className="wrapper" ref={ref1}>
                 {currentItem.map(item => (
-                    <div className="card" key={item.id}>
+                    <div className="card" key={item.id}  animate={inView1}>
                         <img className="card-image" src={item.profile_picture} alt={item.name} />
                         <h2>{item.name}</h2>
                         <p>{item.email}</p>
-                        <button className="custom-btn btn-16">Read More</button>
+                        <button className="custom-btn btn-16" >Read More</button>
                     </div>
                 ))}
             </div>
@@ -84,11 +120,11 @@ const Expert = () =>{
             <h1>Do you know</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero id enim distinctio harum sit, debitis quod voluptate velit repellendus ullam veniam quae a eius totam facere eligendi esse, autem neque!</p>
         </div>
-        <div className="comment">
+        <div className="comment" ref={ref1}>
             <h1>Comment of Customer</h1>
             <p className="p">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit tempora sed ex quae quasi excepturi ad voluptas, aliquid rerum molestias accusamus explicabo quaerat, asperiores quam voluptatem molestiae ullam impedit qui?</p>
             <div className="card-comment">
-                <Card
+                <Card animate={inView1}
                     bordered={false}
                     style={{
                     width: 300,
@@ -104,8 +140,8 @@ const Expert = () =>{
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium, sunt? Quia praesentium maiores, culpa dolore vero laborum accusamus earum dicta consequuntur voluptatem quidem excepturi dolores, autem, doloribus veritatis hic est.
                     </p>
                     <img src="https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg" alt="" />
-                </Card>
-                <Card
+                </Card >
+                <Card  animate={inView1}
                     bordered={false}
                     style={{
                     width: 300,
@@ -120,7 +156,7 @@ const Expert = () =>{
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil amet voluptates fugit facere repellat ipsam, ad repellendus sequi iure eos nobis iste sunt tenetur praesentium accusantium eum cum officia?</p>
                     <img src="https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/anh-den-ngau.jpeg" alt="" />
                 </Card>
-                <Card
+                <Card  animate={inView1}
                     bordered={false}
                     style={{
                     width: 300,
