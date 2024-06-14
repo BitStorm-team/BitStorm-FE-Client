ec5bc1793848670c40f16ad18e0c8baf5f39d664
hiep.bui <hiep.bui@messagemedia.com>
2024-06-14 22:34:40 +0700

    fix router

--- src/api/post.js
+++ src/api/post.js
@@ -61,7 +61,6 @@ export const getLikedPosts = async () => {
     const response = await axios.get(`${API_URL}/liked-posts`, {
       headers
     });
-    console.log("likedPosts",response.data.data);
     return response.data.data;
   } catch (error) {
     console.error('Error checking like status:', error);
