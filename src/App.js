4c2f7012c35cf8796a7f70c9b698749e9f426c9e
Kayz Bui <108621568+kayzbui@users.noreply.github.com>
2024-06-14 17:05:57 +0700

    fix router (#48)

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import { privateRoutes, publicRoutes } from "./utils/routes";
 import WithPublicRoute from "./router/PublicRouter";
 import WithPrivateRoute from "./router/PrivateRouter";
 import NotFound from "./pages/NotFound.jsx";
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {publicRoutes.map((route, index) => (
-          WithPublicRoute(route, index)
-        ))}
-        {privateRoutes.map((route, index) => (
-          WithPrivateRoute(route, index)
-        ))}
-        <Route path="*" element={<NotFound />} />
+        {publicRoutes.map((route, index) =>
+          WithPublicRoute({ ...route }, index, { key: `public_${index}` })
+        )}
+        {privateRoutes.map((route, index) =>
+          WithPrivateRoute({ ...route }, index, { key: `private_${index}` })
+        )}
+       <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
